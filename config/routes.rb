987fd5f9fed40bd82368818a2863f78cd3a34eab
Zac Cassini <zcassini@gmail.com>
2016-05-31 11:24:25 -0400

    a change routes base on authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,25 @@
 Rails.application.routes.draw do
   devise_for :users
-  #get 'welcome/index'
+  get 'welcome/index'
+
+  resources :docs
+
+  authenticated :user do
+    root 'docs#index', as: "authenticated_root"
+  end
+
+  root 'welcome#index'
+
+
+
+
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
-
-  resources :docs
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
